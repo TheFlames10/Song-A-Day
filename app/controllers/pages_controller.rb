3e1286d4f9c609bcc95b0eee5947d593976373ee
TheFlames10 <damiansoccer23@gmail.com>
2025-01-03 13:28:16 -0800

    Optimized calendar page rendering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
 
   def home
     @current_month = Date.current
-    @song_entries = current_user.song_entries.where(date: @current_month.all_month)
+    # Preload all song entries for the month in a single query
+    @song_entries = current_user.song_entries
+                              .where(date: @current_month.beginning_of_month..@current_month.end_of_month)
+                              .index_by(&:date)
   end
 end
