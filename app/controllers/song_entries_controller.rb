863a834bee30871fb6e8d0315ec9d8e460b5662f
TheFlames10 <damiansoccer23@gmail.com>
2025-01-03 13:10:59 -0800

    Added edit functionality to songs

--- app/controllers/song_entries_controller.rb
+++ app/controllers/song_entries_controller.rb
@@ -2,25 +2,31 @@ class SongEntriesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_song_entry, only: [:edit, :update, :destroy]
 
-  def create
-    @song_entry = current_user.song_entries.build(song_entry_params)
-    if @song_entry.save
-      respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to root_path }
-      end
+  def edit
+    @date = @song_entry.date
+    render 'songs/new' # We'll reuse the new song view for editing
+  end
+
+  def update
+    if @song_entry.update(song_entry_params)
+      redirect_to root_path, notice: 'Song updated successfully!'
     else
-      render :new, status: :unprocessable_entity
+      redirect_to edit_song_entry_path(@song_entry), alert: 'Failed to update song.'
     end
+  end
+
+  def destroy
+    @song_entry.destroy
+    redirect_to root_path, notice: 'Song removed successfully!'
   end
 
   private
 
-  def song_entry_params
-    params.require(:song_entry).permit(:date, :song_id, :song_name, :artist_name, :album_cover_url)
-  end
-
   def set_song_entry
     @song_entry = current_user.song_entries.find(params[:id])
+  end
+
+  def song_entry_params
+    params.permit(:date, :song_id, :song_name, :artist_name, :album_cover_url)
   end
 end
--- app/models/song_entry.rb
+++ app/models/song_entry.rb
@@ -5,5 +5,5 @@ class SongEntry < ApplicationRecord
   validates :song_name, presence: true
   validates :artist_name, presence: true
   validates :album_cover_url, presence: true
-  validates :date, uniqueness: { scope: :user_id, message: "already has a song entry" }
+  # validates :date, uniqueness: { scope: :user_id, message: "already has a song entry" }
 end
